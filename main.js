a288a1639a96de713ed23ac78f3f084385e74680
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2024-09-17 06:11:19 +0100

    Create initial test hikues

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Mixed Massagges
 
 // Store Hikues
+let hikues = {
+    line1: ["An ancient pool,","The list of a candle", "This world of dew", "I write, erase, rewrite"],
+    line2: ["A frog jumps in", "Is transferred to antoehr candel", "Is a world of dew,", "Erase again, and then"],
+    line3: ["The sound of water.", "Spring twilight", "and yet, and yet.", "A poppy bloomes"]
+}
 
 // Script to construct Hikeus out of the stored lines
     //Must be randomized
