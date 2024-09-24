fa3b734e5cce8f0f8b546dca283493decc08ee07
AlasdairAnderson <alasdairsw.anderson@gmail.com>
2024-09-24 05:35:58 +0100

    randomisation of hiku lines

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 // Mixed Massagges
 
-// Store Hikues
 let hikues = {
-    line1: ["An ancient pool,","The list of a candle", "This world of dew", "I write, erase, rewrite"],
-    line2: ["A frog jumps in", "Is transferred to antoehr candel", "Is a world of dew,", "Erase again, and then"],
-    line3: ["The sound of water.", "Spring twilight", "and yet, and yet.", "A poppy bloomes"],
+    line1: ["An ancient pool,","The list of a candle,", "This world of dew,", "I write, erase, rewrite"],
+    line2: ["a frog jumps in", "is transferred to antoehr candel", "is a world of dew,", "erase again, and then"],
+    line3: ["the sound of water.", "spring twilight", "and yet, and yet.", "a poppy bloomes"],
     makeHaiku(){
+        //randomise to select each different line
         const firstLine = this.line1[Math.floor(Math.random() * this.line1.length)];
         const secondLine = this.line2[Math.floor(Math.random() * this.line2.length)];
         const thridLine = this.line3[Math.floor(Math.random() * this.line3.length)];
-        //randomise to select each different line
 
         //return concatination of each line
         return `${firstLine} ${secondLine} ${thridLine}`
     }
 }
-
-// Script to construct Hikeus out of the stored lines
-
-    //Must be randomized
 
 console.log(hikues.makeHaiku());
